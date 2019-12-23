aca020868a3d6c56d1653a4c686567112d777f6e
duchiha <duchiha@example.com>
2019-12-24 00:51:34 +0100

    create a shared module

--- sample-app/src/app/app.module.ts
+++ sample-app/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { StarComponent } from './components/star/star.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ProductModule } from './product.module';
 
--- sample-app/src/app/product.module.ts
+++ sample-app/src/app/product.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './services/guards/product-detail.guard';
-import { StarComponent } from './components/star/star.component';
+import { SharedModule } from './shared.module';
 
 
 
@@ -14,16 +12,14 @@ import { StarComponent } from './components/star/star.component';
   declarations: [
     ProductListComponent,
     ConvertToSpacesPipe,
-    ProductDetailComponent,
-    StarComponent
+    ProductDetailComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
